eed8bd9201df40adf45d1589f790b820be001262
Mark Ambro <wowmarkambro@gmail.com>
2021-03-19 13:13:43 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          HOME
+        </Route>
+        <Route path="/dashboard">Dashboard</Route>
+        <Route path="/transactions">All Transactions</Route>
+        <Route>
+          <h1>Error</h1>
+          <p>Page does not exists.</p>
+        </Route>
+      </Switch>
     </div>
   );
 }

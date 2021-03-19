a3467403eff021485131d407d7cc70c4b95c82d0
Mark Ambro <wowmarkambro@gmail.com>
2021-03-19 13:55:35 -0500

    working on styling topnavbar

--- src/components/common/topnavbar/TopNavBar.js
+++ src/components/common/topnavbar/TopNavBar.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import styles from './topnavbar.module.css';
 
 export default function TopNavBar() {
   return (
-    <header style={{ width: '100%' }}>
+    <header className={styles.header}>
       <nav className={styles.navbar__main}>
         <div className={styles.navbar__logo_wrapper}>
-          <img src="/assets/images/logo.png" alt="rosemary" />
+          <Link to="/">
+            <img src="/assets/images/logo.png" alt="rosemary" />
+          </Link>
         </div>
-        <ul className={styles.navbar__links}>
+        <ul className={styles.navbar__navlist}>
           <li>
-            <NavLink to="/dashboard">Dashboard</NavLink>
+            <NavLink className={styles.navbar__links} to="/dashboard">
+              Dashboard
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/transactions">Transactions</NavLink>
+            <NavLink className={styles.navbar__links} to="/transactions">
+              Transactions
+            </NavLink>
           </li>
-          <li>3</li>
-          <li>4</li>
         </ul>
+        <div>
+          <button>Login</button>
+        </div>
       </nav>
     </header>
   );

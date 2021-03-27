6778a3f6b8bd41e412e51ebbbec48f6a05a3f8de
qnf613 <gurwls613@gmail.com>
2021-03-27 18:08:39 -0500

    added teamsection with placeholders

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import TeamSection from '../components/common/homepage_contents/team_section';
 import Layout from '../components/common/layout/layout';
 
 export default function Home() {
-  return <Layout>homepage</Layout>;
+  return <Layout>Homepage<TeamSection/></Layout>;
 }

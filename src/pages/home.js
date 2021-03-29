152459a9054dcf9992e60c58167c9ffd55810570
ylei1088 <57201889+ylei1088@users.noreply.github.com>
2021-03-29 12:20:32 -0500

    added home page features section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import HomeFeatures from '../components/common/homepage_contents/home-features';
 import TeamSection from '../components/common/homepage_contents/team_section';
 import Layout from '../components/common/layout/layout';
 
 export default function Home() {
-  return <Layout>Homepage<TeamSection/></Layout>;
+  return (
+    <Layout>
+      Homepage
+      <HomeFeatures />
+      <TeamSection />
+    </Layout>
+  );
 }

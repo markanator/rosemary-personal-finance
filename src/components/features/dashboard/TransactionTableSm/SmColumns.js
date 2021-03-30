93c9dd3643c990961d44bfec178024767727f046
Mark Ambro <wowmarkambro@gmail.com>
2021-03-30 14:42:03 -0500

    adding react-table architecture

---
+++ src/components/features/dashboard/TransactionTableSm/SmColumns.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+  {
+    Header: 'Type',
+    accessor: 'trx_type',
+  },
+  {
+    Header: 'Date',
+    accessor: 'trx_date',
+  },
+  {
+    Header: 'Details',
+    accessor: 'trx_details',
+  },
+  {
+    Header: 'Category',
+    accessor: 'trx_category',
+  },
+  {
+    Header: 'Amount',
+    accessor: 'trx_amount',
+  },
+  {
+    Header: 'Balance',
+    accessor: 'running_bal',
+  },
+  {
+    Header: 'Options',
+    accessor: 'id',
+  },
+];
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/common/layout/layout';
+import DashboardTrxTable from '../components/features/dashboard/TransactionTableSm';
 
 export default function DashBoard() {
   return (
     <Layout>
-        Dash Board.
+      Dash Board.
+      <DashboardTrxTable />
     </Layout>
   );
 }

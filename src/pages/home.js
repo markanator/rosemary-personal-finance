2719c38f83c7ca4f06fea3c758947c70d514de5e
ylei1088 <57201889+ylei1088@users.noreply.github.com>
2021-03-29 19:20:25 -0500

    Homepage features section (#22)
    * added teamsection with placeholders
    
    * added home page features section
    
    Co-authored-by: qnf613 <gurwls613@gmail.com>

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import HomeFeatures from '../components/common/homepage_contents/home-features';
+import TeamSection from '../components/common/homepage_contents/team_section';
 import Layout from '../components/common/layout/layout';
 
 export default function Home() {
-  return <Layout>homepage</Layout>;
+  return (
+    <Layout>
+      Homepage
+      <HomeFeatures />
+      <TeamSection />
+    </Layout>
+  );
 }

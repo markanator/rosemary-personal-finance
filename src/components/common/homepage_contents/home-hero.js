49419b9fb76b333f5a45090a6b0f37e8a3c07218
Mark Ambro <wowmarkambro@gmail.com>
2021-03-29 20:26:24 -0500

    fixing styles

--- src/components/common/homepage_contents/home-hero.js
+++ src/components/common/homepage_contents/home-hero.js
@@ -1,35 +1,55 @@
-import React from 'react';
+// image from @f7photo https://unsplash.com/photos/lhltMGdohc8
+import React, { useState } from 'react';
+import ModalVideo from 'react-modal-video';
+import './home_hero.scss';
 
 export default function HomeHero() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <section>
       <div className="hero__main">
-        <div className="left__side">
-          <img
-            src="https://images.unsplash.com/photo-1567427017947-545c5f8d16ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1295&q=80"
-            alt="placeholder"
-            width="635px"
-            height="512px"
-          />
-        </div>
-        <div className="right__side">
-          <div>
-            <p>About us</p>
-            <h1>
-              Customized Personal <br />
-              Financial and Money
-              <br />
-              Management
-            </h1>
-            <p>
-              Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
-              nam no suscipit quaerendum. At nam minimum ponderum. Est audiam
-              animal molestiae te. Ex duo eripuit mentitum.
-            </p>
-            <button>Sign Up</button>
+        <div className="hero__wrapper">
+          <div className="left__side">
+            <button type="button" onClick={() => setOpenModal(true)}>
+              <img
+                src="https://images.unsplash.com/photo-1567427017947-545c5f8d16ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1295&q=80"
+                className="hero__image"
+                alt="jar with coins - piggy bank"
+                width="635px"
+                height="512px"
+              />
+            </button>
+          </div>
+          <div className="right__side">
+            <div>
+              <p className="--about">About us</p>
+              <h1 className="--header">
+                Customized Personal <br />
+                Financial and Money
+                <br />
+                Management
+              </h1>
+              <p className="--blurb">
+                Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
+                nam no suscipit quaerendum. At nam minimum ponderum. Est audiam
+                animal molestiae te. Ex duo eripuit mentitum.
+              </p>
+              <button type="button" className="--signup">
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
+      <ModalVideo
+        channel="youtube"
+        controls={false}
+        showinfo={false}
+        autoplay={true}
+        isOpen={openModal}
+        videoId="MJy5H1cC0jw"
+        onClose={() => setOpenModal(false)}
+      />
     </section>
   );
 }

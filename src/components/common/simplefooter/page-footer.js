f1ee16d8805f21675777092398bcc563ef358f23
Ricarto <57201883+qnf613@users.noreply.github.com>
2021-03-29 19:20:46 -0500

    added navigation to footer (#20)

--- src/components/common/simplefooter/page-footer.js
+++ src/components/common/simplefooter/page-footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import "./footer.css"
+import { NavLink, Link } from 'react-router-dom';
+import '../simplefooter/footer.css';
 
 function PageFooter() {
-    const year = new Date().getFullYear();
-    return (<footer className="page-footer">@ TeamNamePlaceholder, {year}</footer>);
+  const year = new Date().getFullYear();
+  return (
+    <footer className="page__footer">
+      <nav className="navbar__main">
+        <div className="navbar__logo_wrapper">
+          <Link to="/">
+            <img src="/assets/images/logo.png" alt="rosemary" />
+          </Link>
+        </div>
+
+        <ul className="navbar__navlist">
+          <li>
+            <NavLink className="navbar__links" to="/dashboard">
+              Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navbar__links" to="/transactions">
+              Transactions
+            </NavLink>
+          </li>
+        </ul>
+        <div className="navbar__copyright">
+          Copyright ⓒ {year}. Rosemary. All rights reserved.
+        </div>
+      </nav>
+    </footer>
+  );
 }
 
 export default PageFooter;

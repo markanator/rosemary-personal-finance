7ed6b8868d40ee0067b2d7c904576996a2ec84ef
Mark Ambrocio <wowmarkambro@gmail.com>
2021-04-06 14:39:31 -0500

    Feat/dashboard transaction table (#26)
    * adding react-table architecture
    
    * implemented basic table with styles
    
    * date formatted, desc split, running balance added
    
    * finished pagination styles
    
    * added column filter
    
    * styled and MVP functionality complete
    
    * fixed spacing issue
    
    * fixing navigation issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 //locals
 import App from './App';
 import './styles/globalStyles.css';
 import './styles/cssreset.css';
-import './styles/videoModal.scss';
+import './styles/index.scss';
 
 ReactDOM.render(
   <Router>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '../components/common/layout/layout';
+import DashboardTableSection from '../components/features/dashboard/TransactionTableSm';
 
 export default function DashBoard() {
   return (
     <Layout>
-        Dash Board.
+      <section>
+      Dash Board.
+      <DashboardTableSection />
+      </section>
     </Layout>
   );
 }

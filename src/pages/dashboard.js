e81c084caa61f6d3fb06de71519af01c6519fc09
Mark Ambro <wowmarkambro@gmail.com>
2021-04-06 20:23:18 -0500

    fixed formating on dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PieChartRender from '../components/common/dashboard_contents/pie-chart';
 import DashboardTableSection from '../components/features/dashboard/TransactionTableSm';
 import Layout from '../components/common/layout/layout';
-import "../styles/dashboard.css";
+import '../styles/dashboard.css';
 
 export default function DashBoard() {
   return (
     <Layout>
       <section className="pie_section">
         <div className="pie_wrapper">
-          <PieChartRender title="CashFlow"/>
-          <PieChartRender title="Spending"/>
+          <PieChartRender title="CashFlow" />
+          <PieChartRender title="Spending" />
         </div>
         <DashboardTableSection />
       </section>
--- src/pages/transaction.js
+++ src/pages/transaction.js
@@ -2,9 +2,5 @@ import React from 'react';
 import Layout from '../components/common/layout/layout';
 
 export default function Transaction() {
-  return (
-    <Layout>
-        Transaction.
-    </Layout>
-  );
+  return <Layout>Transaction.</Layout>;
 }

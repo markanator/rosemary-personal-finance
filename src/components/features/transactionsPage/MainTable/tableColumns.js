37a43a3145a7661e1a46a20a836e86ffd7b154f1
Mark Ambro <wowmarkambro@gmail.com>
2021-04-06 21:04:56 -0500

    basic component setup

---
+++ src/components/features/transactionsPage/MainTable/tableColumns.js
@@ -0,0 +1,42 @@
+import dayjs from 'dayjs';
+import formatMoney from '../../../../utils/formatMoney';
+
+export const MainTableColumns = [
+  {
+    Header: 'Type',
+    accessor: 'trx_type',
+  },
+  {
+    Header: 'Date',
+    accessor: 'trx_date',
+    Cell: ({ value }) => dayjs(value).format('MM/DD/YY').toString(),
+    disableFilters: true,
+  },
+  {
+    Header: 'Details',
+    accessor: 'trx_details',
+    Cell: ({ value }) => `${value.slice(0, 57)}...`,
+    disableFilters: true,
+  },
+  {
+    Header: 'Category',
+    accessor: 'trx_category',
+  },
+  {
+    Header: 'Amount',
+    accessor: 'trx_amount',
+    Cell: ({ value }) => {
+      return formatMoney(value * 100);
+    },
+    disableFilters: true,
+  },
+  {
+    Header: 'Balance',
+    accessor: 'running_bal',
+    disableFilters: true,
+  },
+  // {
+  //   Header: '_',
+  //   accessor: '_id',
+  // },
+];
--- src/pages/transaction.js
+++ src/pages/transaction.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Layout from '../components/common/layout/layout';
+import FullTable from '../components/features/transactionsPage/MainTable';
 
 export default function Transaction() {
   return (
     <Layout>
-        Transaction.
+      <main className="transaction__main">
+        <h1 className="--header">Transaction.</h1>
+        <div className="transaction__wrapper">
+          <aside className="transaction__sidebar">sidebar</aside>
+          <section className="transaction__table">
+            <FullTable />
+          </section>
+        </div>
+      </main>
     </Layout>
   );
 }

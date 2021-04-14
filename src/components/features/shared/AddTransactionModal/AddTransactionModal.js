abcf8daceb8d7a9dce84b115c3aa255331ce9907
Mark Ambro <wowmarkambro@gmail.com>
2021-04-13 19:43:44 -0500

    added materialui modal to add trx

---
+++ src/components/features/shared/AddTransactionModal/AddTransactionModal.js
@@ -0,0 +1,37 @@
+import { makeStyles, Modal } from '@material-ui/core';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function AddTransactionModal({ handleClose, open }) {
+  const classes = useStyles();
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      className={classes.modal}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <div className={classes.paper}>
+        <h2 id="simple-modal-title">Text in a modal</h2>
+        <p id="simple-modal-description">
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </p>
+      </div>
+    </Modal>
+  );
+}
